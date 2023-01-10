2533ed45df1487f21d2dc2a3539e98577630d8b0
ESPORTS-140\cre <t-shirai@oca.ac.jp>
2023-01-10 15:16:44 +0900

    スタート地点

--- storage/framework/views/6b5f1e4b44e3f29daf039d2d865f1fec0f467f05.php
+++ storage/framework/views/6b5f1e4b44e3f29daf039d2d865f1fec0f467f05.php
@@ -137,19 +137,6 @@ unset($__errorArgs, $__bag); ?>
     </footer>
     <form action="/tasks" method="post" class="mt-10">
   <?php echo csrf_field(); ?>
- 
-  <div class="flex flex-col items-center">
-    <label class="w-full max-w-3xl mx-auto">
-        <input
-            class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-4 pl-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            placeholder="洗濯物をする..." type="text" name="task_name" />
-    </label>
- 
-    <button type="submit" class="mt-8 p-4 bg-slate-800 text-white w-full max-w-xs hover:bg-slate-900 transition-colors">
-        追加する
-    </button>
-  </div>
- 
 </form>
 </body>
  
